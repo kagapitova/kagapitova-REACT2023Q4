8fa61cc121791cbb79f9160026072ba5289221d9
kseniaagapitova <kagapitova@inbox.ru>
2023-11-11 16:48:07 +0200

    feat: add search test

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Search.module.css";
+import { useAppContext } from "./AppProvider";
 
 interface SearchProps {
   onSearch: (searchTerm: string) => Promise<void>;
 }
 
 const Search: React.FC<SearchProps> = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState<string>("");
+  const { searchTerm: contextSearchTerm, updateSearchTerm } = useAppContext();
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const savedSearchTerm = localStorage.getItem("searchTerm");
     if (savedSearchTerm) {
-      setSearchTerm(savedSearchTerm);
+      updateSearchTerm(savedSearchTerm);
       onSearch(savedSearchTerm).finally(() => setLoading(false));
     }
   }, []);
 
   const handleSearch = () => {
-    const trimmedSearchTerm = searchTerm.trim();
+    const trimmedSearchTerm = contextSearchTerm.trim();
     setLoading(true);
     localStorage.setItem("searchTerm", trimmedSearchTerm);
     onSearch(trimmedSearchTerm).finally(() => setLoading(false));
@@ -30,8 +31,8 @@ const Search: React.FC<SearchProps> = ({ onSearch }) => {
         className={styles.input}
         type="text"
         placeholder="Search..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        value={contextSearchTerm}
+        onChange={(e) => updateSearchTerm(e.target.value)}
       />
       <button onClick={handleSearch}>Search</button>
       <div className={loading ? styles.showLoader : styles.loader} />
--- tests/Search.test.tsx
+++ tests/Search.test.tsx
@@ -1,11 +1,13 @@
-// Search.test.tsx
 import { render } from "@testing-library/react";
 import Search from "../src/Search";
 import "@testing-library/jest-dom";
+import { AppProvider } from "../src/AppProvider";
 
 test("Search component renders correctly", () => {
   const { getByText, getByPlaceholderText } = render(
-    <Search onSearch={() => {}} />,
+    <AppProvider>
+      <Search onSearch={() => {}} />
+    </AppProvider>,
   );
   const inputElement = getByPlaceholderText("Search...");
   const buttonElement = getByText("Search");
